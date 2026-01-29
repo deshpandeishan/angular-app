6f618284183691a9df46f3bcbd4f02b3284cd2a9
Ishan Deshpande <deshpandeishan75@gmail.com>
2026-01-29 19:04:08 +0530

    amade responsive for mobile

--- src/app/food/food-table/food-table.ts
+++ src/app/food/food-table/food-table.ts
@@ -3,12 +3,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
     selector: 'app-food-table',
     templateUrl: './food-table.html',
-    styleUrl: './food-table.css',
+    styleUrls: ['./food-table.css'],
 })
 export class FoodTable {
     @Output() addFood = new EventEmitter<void>();
     @Output() editFood = new EventEmitter<void>();
-    
+
     goToAdd() {
         this.addFood.emit();
     }
