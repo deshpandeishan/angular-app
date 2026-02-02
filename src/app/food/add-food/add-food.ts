f771538d054ffb0c82c411b9ce0c4f264ff8da66
Ishan Deshpande <deshpandeishan75@gmail.com>
2026-02-02 17:44:41 +0530

    add tooltip to view table button

--- src/app/food/add-food/add-food.ts
+++ src/app/food/add-food/add-food.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Output, NgZone } from '@angular/core';
+import { Component, EventEmitter, Output, NgZone, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FoodService, FoodItem } from '../../services/food';
+import * as bootstrap from 'bootstrap';
 
 @Component({
     selector: 'app-add-food',
@@ -14,9 +15,14 @@ export class AddFood {
     @Output() viewTable = new EventEmitter<void>();
     @Output() itemAdded = new EventEmitter<void>();
 
+    @ViewChild('viewTableBtn', { static: false }) viewTableBtn!: ElementRef;
+
     food: FoodItem = { foodName: '', price: 0, category: '' };
     imagePreview: string | null = null;
     selectedImage: File | null = null;
+
+    showToast = false;
+    private tooltip: bootstrap.Tooltip | null = null;
 
     constructor(private foodService: FoodService, private zone: NgZone) {}
 
@@ -48,11 +54,38 @@ export class AddFood {
 
         this.foodService.addFood(formData, { responseType: 'text' as 'json' }).subscribe(() => {
             this.zone.run(() => {
-                this.food = { foodName: '', price: 0, category: ''};
+                this.food = { foodName: '', price: 0, category: '' };
                 this.imagePreview = null;
                 this.selectedImage = null;
                 this.itemAdded.emit();
+
+                this.showToast = true;
+
+                setTimeout(() => {
+                    this.showToast = false;
+                    this.showTooltip();
+                }, 1200);
             });
         });
+    }
+
+    showTooltip() {
+        if (!this.viewTableBtn) return;
+
+        this.tooltip?.dispose();
+
+        this.tooltip = new bootstrap.Tooltip(this.viewTableBtn.nativeElement, {
+            title: 'View the newly added item here',
+            placement: 'bottom',
+            trigger: 'manual'
+        });
+
+        this.tooltip.show();
+
+        setTimeout(() => {
+            this.tooltip?.hide();
+            this.tooltip?.dispose();
+            this.tooltip = null;
+        }, 2500);
     }
 }
